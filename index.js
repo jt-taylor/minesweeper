ef13b546faf7350e9d97abf9a0ccff4cc72aa411
Jackson Tyler Taylor <44556890+jt-taylor@users.noreply.github.com>
2022-04-01 16:00:46 -0700

    precomputes adjacent mines just need to hook up handling the mouseclick to update the cells state

--- index.js
+++ index.js
@@ -6,8 +6,18 @@ var components = {
     alive: true,
     colors : {1: 'blue', 2: 'green', 3: 'red', 4: 'purple', 5: 'turquoise', 6: 'brown', 7: 'pink', 8: 'grey'}
 }
-var board;
+/*
+0 for empty
+1 for bomb present
+2 for display / clicked / show adjacent bomb count
+if i want to do marking
+3 for marked w/ bomb
 
+going to use a seperate array for handling the how many bombs are near
+*/
+var board;
+var boardDisp;
+var boardAdj;
 
 document.addEventListener("DOMContentLoaded", function() {
     startGame();
@@ -20,12 +30,25 @@ function startGame() {
         for (j = 0; j < components.num_col; j++)
             board[i][j] = 0;
     }
+    boardDisp = new Array(components.num_rows);
+    for (i = 0; i < components.num_rows; i++) {
+        boardDisp[i] = new Array(components.num_col);
+        for (j = 0; j < components.num_col; j++)
+            boardDisp[i][j] = 0;
+    }
+    boardAdj = new Array(components.num_rows);
+    for (i = 0; i < components.num_rows; i++) {
+        boardAdj[i] = new Array(components.num_col);
+        for (j = 0; j < components.num_col; j++)
+            boardAdj[i][j] = 0;
+    }
     placeBombs(board);
     // init board
     //TODO: Async the game logic out of the ui display
     //place bombs
     let tab = createTable();
     document.getElementById('gameBoard').appendChild(tab);
+    countAdjacentBombs();
 }
 
 function createTable() {
@@ -77,4 +100,151 @@ function printBoard() {
             console.log(board);
         }
     }
+}
+function updateBoard() {
+    for (y = 0; y < components.num_rows; y++) {
+        for (x = 0; x < components.num_col;x++) {
+            let tmp = document.getElementById(y + '-' + x);
+            if (tmp) {
+                switch (board[y][x]) {
+                    case 0: {
+                        tmp.style.color = "brown";
+                        tmp.style.backgroundColor = "brown";
+                        tmp.innerHTML = "";
+                        break;
+                    }
+                    case 1: {
+                        /*
+                        tmp.style.color= 'red';
+                        tmp.style.backgroundColor = "red";
+                        tmp.textContent = components.bomb;
+                        */
+                        break;
+                    }
+                    case 2: {
+                        tmp.style.color = "black";
+                        tmp.style.backgroundColor = "white";
+                        if (boardAdj[y][x])
+                            tmp.innerHTML = boardAdj[y][x];
+                        else   
+                            tmp.innerHTML = "";
+                    }
+                    default: {
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
+function updateBoardDebug() {
+    for (y = 0; y < components.num_rows; y++) {
+        for (x = 0; x < components.num_col;x++) {
+            let tmp = document.getElementById(y + '-' + x);
+            if (tmp) {
+                switch (board[y][x]) {
+                    case 0: {
+                        tmp.style.color = "brown";
+                        tmp.style.backgroundColor = "brown";
+                        tmp.innerHTML = "";
+                        break;
+                    }
+                    case 1: {
+                        tmp.style.color= 'red';
+                        tmp.style.backgroundColor = "red";
+                        tmp.textContent = components.bomb;
+                        break;
+                    }
+                    default: {
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
+
+//TODO : 
+/*
+function updateBoard() {
+    for (y = 0; y < components.num_rows; y++) {
+        for (x = 0; x < components.num_col;x++) {
+            let tmp = document.getElementById(y + '-' + x);
+            if (tmp) {
+                switch (boardDisp[y][x]) {
+                    case 0: {
+                        break;
+                    }
+                    case 1: {
+                        break;
+                    }
+                    default: {
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
+*/
+
+function countAdjacentBombs() {
+    for (y = 0; y < components.num_rows ; y++) {
+        for (x = 0 ; x < components.num_col ; x++) {
+            if (board[y][x] == 1)  {
+                //console.log(y, x);
+                if (y == 0 && x == 0) {
+                    boardAdj[y][x+1] += 1;
+                    boardAdj[y+1][x+1] += 1;
+                    boardAdj[y+1][x] += 1;
+                } else if (y == 0 && x == components.num_col - 1) {
+                    boardAdj[y+1][x] += 1;
+                    boardAdj[y][x-1] += 1;
+                    boardAdj[y+1][x-1] += 1;
+                } else if (y == components.num_rows - 1 && x == 0) {
+                    boardAdj[y-1][x] += 1;
+                    boardAdj[y][x+1] += 1;
+                    boardAdj[y-1][x+1] += 1;
+                } else if (y == components.num_rows - 1 && x == components.num_col - 1) {
+                    boardAdj[y-1][x] += 1;
+                    boardAdj[y][x-1] += 1;
+                    boardAdj[y-1][x-1] += 1;
+                } else if (y == 0) {
+                    boardAdj[y+1][x-1] += 1;
+                    boardAdj[y+1][x] += 1;
+                    boardAdj[y+1][x+1] += 1;
+                    boardAdj[y][x+1] += 1;
+                    boardAdj[y][x-1] += 1;
+                } else if (x == 0) {
+                    boardAdj[y+1][x+1] += 1;
+                    boardAdj[y][x+1] += 1;
+                    boardAdj[y-1][x+1] += 1;
+                    boardAdj[y+1][x] += 1;
+                    boardAdj[y-1][x] += 1;
+                } else if (y == components.num_rows - 1) {
+                    boardAdj[y-1][x-1] += 1;
+                    boardAdj[y-1][x] += 1;
+                    boardAdj[y-1][x+1] += 1;
+                    boardAdj[y][x+1] += 1;
+                    boardAdj[y][x-1] += 1;
+                } else if (x == components.num_col - 1) {
+                    boardAdj[y-1][x-1] += 1;
+                    boardAdj[y][x-1] += 1;
+                    boardAdj[y+1][x-1] += 1;
+                    boardAdj[y-1][x] += 1;
+                    boardAdj[y+1][x] += 1;
+                }
+                else { // not a corner / edge square
+                    boardAdj[y-1][x-1] += 1;   
+                    boardAdj[y-1][x] += 1;   
+                    boardAdj[y-1][x+1] += 1;   
+                    boardAdj[y][x-1] += 1;   
+                    boardAdj[y][x+1] += 1;   
+                    boardAdj[y+1][x-1] += 1;   
+                    boardAdj[y+1][x] += 1;   
+                    boardAdj[y+1][x+1] += 1;   
+                }
+            }
+        }
+    }   
 }
